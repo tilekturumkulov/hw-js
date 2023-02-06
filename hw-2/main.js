c05f4ff9143a3d1c16e4188f28dc2753fd8383f4
tilekturumkulov <turumkuloff@gmail.com>
2023-02-06 21:14:50 +0600

    2-hw

---
+++ hw-2/main.js
@@ -0,0 +1,135 @@
+// 1
+
+// let num = 22
+
+// console.log(num)
+
+// 2
+
+// let a = 10
+//     b = 2
+//     c = 5
+//     sum = a + b + c
+
+// console.log(sum)
+
+// 3
+
+// let d = 100
+
+// console.log(d * 2)
+
+// 4
+
+// let e = 67
+
+// console.log(67 % 7)
+
+// 5
+
+// let a = 10
+//     b = 2
+//     sum = a + b
+//     raz = a - b
+//     pr = a * b
+//     ch = a / b
+
+// console.log(sum, raz, pr, ch)
+
+// 6
+
+// let c = 15
+//     d = 2
+//     result = c + d
+
+// console.log(result)
+
+// 7
+
+// let a = 30
+//     b = 15
+//     c = a + b
+
+// console.log(c % 4)
+
+// 8
+
+// let a = 17
+//     b = 10
+//     c = a - b
+//     d = 3
+//     result = c + d
+
+// console.log(result)
+
+// 9
+
+// let a = 10
+//     b = 99
+//     result = a + b
+
+// console.log(result)
+
+// 10
+
+// let name = "лет"
+//     number = 10
+//     result = name + number
+
+// console.log(result)
+
+// 11
+
+// let num = 123
+
+// console.log(String(num))
+
+// 12
+
+// let a = true
+
+// console.log(a * 1)
+// console.log(+a)
+
+// 13
+
+// let a = '12s'
+//     b = '12.2'
+
+// console.log(a * 1, b * 1)
+// console.log(Number(a), Number(b))
+// console.log(+a, +b)
+
+// 14
+
+// let a = null
+//     b = undefined
+
+// console.log(a * 1, b * 1)
+// console.log(Number(a), Number(b))
+// console.log(+a, +b)
+
+// 15
+
+// let t = true
+//     f = false
+
+// console.log(t * 1, f * 1)
+// console.log(Number(t), Number(f))
+// console.log(+t, +f)
+
+// 16
+
+// let a = 10
+//     b = 5
+//     sum = a - b
+
+// console.log(sum) бинарный
+
+// 17
+
+// let a = 5
+//     a = -a
+
+// console.log(a) унарный
+
