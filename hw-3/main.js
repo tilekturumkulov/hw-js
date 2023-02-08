bb4ef8e960c42be6f27312dab6d0fb1b98b3b51a
tilekturumkulov <turumkuloff@gmail.com>
2023-02-08 23:24:29 +0600

    hw-2

---
+++ hw-3/main.js
@@ -0,0 +1,77 @@
+// 1
+
+// let a ='34'
+
+// console.log(+a)
+// console.log(a * 1)
+// console.log(Number(a))
+
+// 2
+
+// let b = 29.92
+
+// console.log(Math.floor(b)) Округляет в меньшую сторону
+// console.log(Math.ceil(b)) Округляет в большую сторону
+// console.log(Math.round(b)) Округляет в ближайщую сторону
+// console.log(b.toFixed()) Округляет в ближайшую сторону и переводит в строку
+
+// 3
+
+// console.log(Math.random(1) * 10)
+// console.log(Math.random(30) * 90)
+// console.log(Math.random(200) * -500)
+
+// 4
+
+// let d = 'aBRAcADBRA'
+
+// console.log(d.toUpperCase())
+
+// 5
+
+// let d = 'Hello World'
+
+// console.log(d[0])
+// console.log(d[1])
+// console.log(d[9])
+// console.log(d[10])
+// console.log(d[d.length - 11])
+// console.log(d[d.length - 10])
+// console.log(d[d.length - 2])
+// console.log(d[d.length - 1])
+// console.log(d.substring(0,1))
+// console.log(d.substring(1,2))
+// console.log(d.substring(9,10))
+// console.log(d.substring(10,11))
+// console.log(d.substr(0,1))
+// console.log(d.substr(1,1))
+// console.log(d.substr(9,1))
+// console.log(d.substr(10,1))
+
+// 6
+
+// let e = 'Hello World'
+
+// console.log(e.length)
+// console.log(e.indexOf('d') + 1)
+
+// 7
+
+// let f ='abracaDabra'
+
+// console.log(f[0]) выводит элемент под этим индексом
+// console.log(f.indexOf('abr')) выводит индекс элемента в строке
+// console.log(f[f.length - 11]) рассчитывает длину и отнимает определенное число для нахождения индекса т.е (11 - 11 = 0)
+// console.log(f.includes('abr')) проверяет содержит ли в себе строка такой порядок элементов 
+// console.log(f.startsWith('a')) проверяет начинаеться ли строка с этого элемента
+// console.log(f.endsWith('a')) проверяет заканчиваеться ли строка на этот элемент
+
+
+
+// 8
+
+// let g = 'Propagination'
+
+// console.log(g.slice(3)) выводит все элементы начиная с 3го элемента не включительно
+// console.log(g.substring(0,5)) выводит элементы от индкса первого значения до индекса второго значения
+// console.log(g.substr(1,5)) выводит кол-во элементов указанное во втором значении от индкса первого значения
