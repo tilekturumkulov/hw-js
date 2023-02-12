37766d935864e2c33e485c1de48583930de3f6fb
tilekturumkulov <turumkuloff@gmail.com>
2023-02-13 01:09:53 +0600

    hw-5

---
+++ hw-5/main.js
@@ -0,0 +1,69 @@
+// 1
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+
+// console.log(salaries.John + salaries.Ann + salaries.Pete)
+
+// 2
+
+// let obj = {
+//     name:'Ivan', 
+//     age: 26
+// }
+
+// console.log(obj.name)
+
+// 3
+
+// let obj = {
+//     name:'Ivan',
+//     age: 26
+// }
+
+// console.log(obj.age - 1 + 5)
+
+// 4
+
+// let obj = {
+//     name:'Ivan',
+//     age: 26,
+//     student: true
+// }
+
+// obj.student = false
+
+// console.log(obj)
+
+// 5
+
+// let obj = {
+//     name:'ABRACADABRA',
+//     age: 15,
+//     student: true
+// }
+
+// console.log(obj.name.includes('BRAC'))
+
+// 6
+
+// 7
+
+// let obj = {
+//     name:'Ivan',
+//     age: 26,
+//     student: true
+// }
+
+// let obj2 = JSON.parse(JSON.stringify(obj))
+
+// obj2.name = 'Tilek'
+// obj2.age = 32
+// obj2.student = false
+
+// console.log(obj2,obj)
+
+// 8
