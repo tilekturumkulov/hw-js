29a26fb87a123caa342ba9937d6ffc5d92164008
tilekturumkulov <turumkuloff@gmail.com>
2023-02-16 17:31:47 +0600

    hw-4 dopolnenie

--- hw-4/main.js
+++ hw-4/main.js
@@ -2,18 +2,47 @@
 
 // let a = 'test'
 
-// if (a === 'tesnt') {
+// if (a === 'test') {
 //     console.log('Verno')
 // } else {
 //     console.log('Neverno')
 // }
 
+// a === 'test'? console.log('Verno') : console.log('Neverno')
+
+// switch(a){
+//     case 'test':
+//         console.log('Verno')
+//         break
+//     default:
+//     console.log('Neverno')
+// }
+
+
 // 2
 
 // let number
-//     data = false;
+//     data = true;
 
 // data === true ? number = 2 : number = 3;
+
+// console.log(number)
+
+// if (data === true){
+//     number = 2
+// } else {
+//     number = 3
+// }
+
+// console.log(number)
+
+// switch(data) {
+//     case true:
+//         number = 2
+//     break
+//     default:
+//         number = 3
+// }
 
 // console.log(number)
 
@@ -50,7 +79,7 @@
 //       alert( 'Будем рады Вас видеть!' );
 //     break
 //     case '12':
-//       alert( 'У нвс обед' );
+//       alert( 'У нaс обед' );
 //     break
 //     default:
 //       alert( "Нет таких значений" );
@@ -58,9 +87,23 @@
 
 // time >= 9 && time < 12 ? alert( 'Будем рады Вас видеть!' ): time >= 13 && time < 23 ? alert( 'Будем рады Вас видеть!' ): time >= 23 && time < 24 ? alert( 'В это время мы не работаем' ): time >= 0 && time < 9 ? alert( 'В это время мы не работаем' ): time > 11.9 && time < 13 ? alert( 'У нвс обед' ) : alert( "Нет таких значений" );
 
+// if ( time >= 9 && time < 12){
+//     alert( 'Будем рады Вас видеть!' )
+// } else if (time >= 13 && time < 23 ){
+//     alert( 'Будем рады Вас видеть!' )
+// } else if ( time >= 23 && time < 24 ){
+//     alert( 'В это время мы не работаем')
+// } else if( time >= 0 && time < 9 ){
+//     alert( 'В это время мы не работаем' )
+// } else if ( time > 11.9 && time < 13 ){
+//     alert( 'У нвс обед' )
+// } else{
+//     alert( "Нет таких значений" )
+// }
+
 // 4
 
-// let m = 50;
+// let m = 51;
 // let n;
 
 // if (m > 50) {
@@ -69,11 +112,21 @@
 //     n = "Маленькое"
 // }
 
+// m > 50 ? n = "Большое" : n = "Маленькое";
+
+// switch (true) {
+//     case (m > 50):
+//         n = "Большое"
+//         break;
+//         default:
+//             n = "Маленькое"
+// }
+
 // console.log(n)
 
 // 5
 
-// let a = 1
+// let a = 2
 
 // if (a === 2) {
 //  a = a + 7
@@ -92,6 +145,17 @@
 //     console .log(a / 10)
 // }
 
+// a === 2 || a === 0 ? console.log( a + 7) : console .log(a / 10)
+
+// switch(a){
+//     case 0:
+//     case 2:
+//         a = a + 7
+//         console.log(a)
+//         break;
+//     default:
+//         console .log(a / 10)
+// }
 
 // 6
 
@@ -114,10 +178,12 @@
 //     console.log('Нет')
 // }
 
+// age ? console.log('Есть') : console.log('Нет')
+
 // 7
 
-// let a = 1
-//     b = 3
+let a = 1
+    b = 3
 
 // if (a <= 1 && b >= 3) {
 //     console.log(a + b)
@@ -125,27 +191,39 @@
 //     console.log(a - b)
 // }
 
-// if (a <= 1 && b >= 3) {
-//     console.log(a + b)
-// } else {
-//     console.log(a - b)
+// a <= 1 && b >= 3 ? console.log(a + b) : console.log(a - b)
+
+// switch ( true ) {
+//     case (a <= 1 && b >= 3):
+//         console.log(a + b)
+//         break;
+//         default:
+//         console.log(a - b)
 // }
 
-// if (a <= 1 || b >= 3) {
-//     console.log(a + b)
-// } else {
-//     console.log(a - b)
-// }
 
 // 8
 
-// let age = 0
+let age = 18
 
 // if ( age > 0 && age <= 17) {
 //     console.log('Паша еще ребенок!')
 // } else if (age >= 18) {
 //     console.log('Паша стал взрослым!')
 // } else {
+//     console.log('Нет таких значений')
+// }
+
+// age > 0 && age <= 17 ? console.log('Паша еще ребенок!') : age >= 18 ? console.log('Паша стал взрослым!') : console.log('Нет таких значений')
+
+// switch (true) {
+//     case (age > 0 && age <= 17):
+//         console.log('Паша еще ребенок!')
+//         break;
+//     case (age >= 18):
+//         console.log('Паша стал взрослым!')
+//         break;
+//     default:
 //     console.log('Нет таких значений')
 // }
 
@@ -178,9 +256,51 @@
 //       alert( "Нет таких значений" );
 // }
 
+// if (seasons.toLowerCase() === 'декабрь' || seasons.toLowerCase() === 'январь' || seasons.toLowerCase() === 'февраль') {
+//     alert( 'Зима' );
+// } else if (seasons.toLowerCase() === 'март' || seasons.toLowerCase() === 'апрель' || seasons.toLowerCase() === 'май') {
+//     alert( 'Весна' );
+// } else if (seasons.toLowerCase() === 'июнь' ||seasons.toLowerCase() === 'июль' || seasons.toLowerCase() === 'август') {
+//     alert( 'Лето' );
+// } else if (seasons.toLowerCase() === 'сентябрь' || seasons.toLowerCase() === 'октябрь' || seasons.toLowerCase() === 'ноябрь') {
+//     alert( 'Осень' );
+// } else {
+//     alert( 'Нет таких значений' );
+// }
+
+// seasons.toLowerCase() === 'декабрь' || seasons.toLowerCase() === 'январь' || seasons.toLowerCase() === 'февраль' ? alert( 'Зима' ) : seasons.toLowerCase() === 'март' || seasons.toLowerCase() === 'апрель' || seasons.toLowerCase() === 'май' ? alert( 'Весна' ) : seasons.toLowerCase() === 'июнь' || seasons.toLowerCase() === 'июль' || seasons.toLowerCase() === 'август' ? alert( 'Лето' ) : seasons.toLowerCase() === 'сентябрь' || seasons.toLowerCase() === 'октябрь' || seasons.toLowerCase() === 'ноябрь' ? alert( 'Осень' ) : alert( 'Нет таких значений' );
+
 // 10
 
 // let time = prompt('Введите любое число', [])
 
 // time >= 0 && time <= 15 ? console.log('время относится к четверти часа') : time > 15 && time <= 30 ? console.log('время относится к половине часа') : time > 30 && time <= 45 ? console.log('время относится к 3/4 часа') : time > 45 && time <= 60 ? console.log('время относится к целому часу') : console.log("неизвестный диапазон"); 
 
+// if (time >= 0 && time <= 15) {
+//     console.log('время относится к четверти часа')
+// } else if ( time > 15 && time <= 30 ) {
+//     console.log('время относится к половине часа')
+// } else if ( time > 30 && time <= 45 ) {
+//     console.log('время относится к 3/4 часа')
+// } else if (time > 45 && time <= 60) {
+//     console.log('время относится к целому часу')
+// } else {
+//     console.log("неизвестный диапазон")
+// }
+
+// switch (true) {
+//     case (time >= 0 && time <= 15):
+//         console.log('время относится к четверти часа')
+//         break
+//     case (time > 15 && time <= 30):
+//         console.log('время относится к половине часа')
+//         break
+//     case (time > 30 && time <= 45):
+//         console.log('время относится к 3/4 часа')
+//         break
+//     case (time > 45 && time <= 60):
+//         console.log('время относится к целому часу')
+//         break
+//     default:
+//         console.log("неизвестный диапазон")
+// }
